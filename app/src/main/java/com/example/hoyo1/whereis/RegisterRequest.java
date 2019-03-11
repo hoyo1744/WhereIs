6199c5fd166035a4b4b60cb0846b9c7dafcfa890
hoyo1744 <hoyo1744@naver.com>
2019-03-11 22:15:22 +0900

    Initial commit

---
+++ app/src/main/java/com/example/hoyo1/whereis/RegisterRequest.java
@@ -0,0 +1,31 @@
+package com.example.hoyo1.whereis;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.Map;
+
+public class RegisterRequest extends StringRequest {
+    final static private String url = "http://.php";
+    private Map<String, String> parameters;
+
+    public RegisterRequest(String userId, String userPassword, String userEmail, String userName, String userPhoneNumber, String userGender, Response.Listener<String> listener) {
+        super(Method.POST, url, listener, null);
+
+        parameters = new HashMapmap<>();
+        parameters.put("userId", userId);
+        parameters.put("userPassword", userPassword);
+        parameters.put("userEmail", userEmail);
+        parameters.put("userName", userName);
+        parameters.put("userPhoneNumber", userPhoneNumber);
+        parameters.put("userGender", userGender);
+    }
+
+    @Override
+    public Map<String, String> getParams() {
+        return parameters;
+    }
+}
+
+
+
---
+++ app/src/main/java/com/example/hoyo1/whereis/ValidateRequest.java
@@ -0,0 +1,26 @@
+package com.example.hoyo1.whereis;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.Map;
+
+public class ValidateRequest extends StringRequest {
+    final static private String url="http://.php";
+    private Map<String,String> parameters;
+    public ValidateRequest(String userId, Response.Listener<String> listener) {
+        super(Method.POST, url, listener, null);
+
+        parameters = new HashMapmap<>();
+        parameters.put("userId", userId);
+
+    }
+    @Override
+    public Map<String,String> getParams(){
+        return parameters;
+    }
+
+}
+
+
+
