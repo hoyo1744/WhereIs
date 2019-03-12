05f98ae536e63deeee88a229b1e02c95fc0438de
hoyo1744 <hoyo1744@naver.com>
2019-03-12 21:51:48 +0900

    20190312

--- app/src/main/java/com/example/hoyo1/whereis/LoginActivity.java
+++ app/src/main/java/com/example/hoyo1/whereis/LoginActivity.java
@@ -1,12 +1,24 @@
 package com.example.hoyo1.whereis;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private AlertDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +31,69 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent registerIntent=new Intent(LoginActivity.this,RegisterActivity.class);
                 LoginActivity.this.startActivity(registerIntent);
+            }
+
+
+
+        });
+
+        final EditText idText=(EditText)findViewById(R.id.idText);
+        final EditText passwordText=(EditText)findViewById(R.id.passwordText);
+        final Button loginButton=(Button)findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userId=idText.getText().toString();
+                String userPassword=passwordText.getText().toString();
+
+                Response.Listener<String> responseLister= new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try{
+
+                            JSONObject jsonResponse=new JSONObject(response);
+                            boolean success=jsonResponse.getBoolean("success");
+                            if(success)
+                            {
+                                AlertDialog.Builder builder=new AlertDialog.Builder(LoginActivity.this);
+                                dialog=builder.setMessage("로그인에 성공했습니다.")
+                                        .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which) {
+
+                                                //로그인이 성공한다면 다음씬으로 넘어간다.
+                                                Intent intent=new Intent(LoginActivity.this,MainActivity.class);
+                                                LoginActivity.this.startActivity(intent);
+                                                finish();
+
+                                            }
+                                        })
+                                        .create();
+                                dialog.show();
+
+
+                            }
+                            else
+                            {
+                                AlertDialog.Builder builder=new AlertDialog.Builder(LoginActivity.this);
+                                dialog=builder.setMessage("로그인에 실패했습니다.")
+                                        .setPositiveButton("확인", null)
+                                        .create();
+                                dialog.show();
+                            }
+                        }
+                        catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+
+                LoginRequest loginRequest=new LoginRequest(userId,userPassword,responseLister);
+                RequestQueue queue=Volley.newRequestQueue(LoginActivity.this);
+                queue.add(loginRequest);
+
+
             }
         });
     }
